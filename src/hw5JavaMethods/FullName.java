f8f7577345c5e6a79ecd3c4c6164c4576df83fe7
“Shaki2016” <shaki07@gmail.com>
2024-08-29 18:10:29 -0400

    “update”

--- src/hw5JavaMethods/FullName.java
+++ src/hw5JavaMethods/FullName.java
@@ -2,9 +2,19 @@ package hw5JavaMethods;
 
 public class FullName {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public String fullName(String fName, String lName) {
+		String fullName = fName + " " + lName;
+		System.out.println("Family Member: " + fullName);
+		return fullName;
 	}
 
+	// Main method
+	public static void main(String[] args) {
+		FullName fn = new FullName();
+
+		fn.fullName("Mohammed", "Islam");
+		fn.fullName("Seikh", "Hasina");
+		fn.fullName("Khaleda", "Zia");
+		fn.fullName("Mohammed", "Yunus");
+	}
 }
--- src/hw5JavaMethods/MyFamily.java
+++ src/hw5JavaMethods/MyFamily.java
@@ -1,10 +1,25 @@
 package hw5JavaMethods;
 
 public class MyFamily {
+	// Initialize some int type variables
+	public int child1Age = 10;
+	public int child2Age = 9;
+	public int child3Age = 11;
+	public int child4Age = 2;
 
+	// Implement a return type method
+	public int myFamily() {
+		int sum = child1Age + child2Age + child3Age + child4Age;
+		System.out.println("The sum of my childrens age is: " + sum);
+		return sum;
+
+	}
+
+	// Main method
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		MyFamily family = new MyFamily();
+		family.myFamily();
 	}
 
 }
